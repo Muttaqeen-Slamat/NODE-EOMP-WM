c36d39161ed855fb9c63fbeba9d3f95a1ca44540
Wareezy <warrenjaftha16@gmail.com>
2024-03-01 10:11:14 +0200

    warren response about change

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card">
+    <div class="card" :class="{ 'hovered': isHovered }" @mouseover="toggleHover(true)" @mouseleave="toggleHover(false)">
         <div id="cardTitle" class="card-header bg-success text-white">
             <slot name="cardHeader"></slot>
         </div>
@@ -11,7 +11,17 @@
 
 <script>
     export default {
-        name: 'CardView'
+        name: 'CardView',
+        data() {
+            return {
+                isHovered: false
+            };
+        },
+        methods: {
+            toggleHover(state) {
+                this.isHovered = state;
+            }
+        }
     }
 </script>
 
@@ -19,9 +29,14 @@
     .card {
         width: 18rem;
         margin: .5rem;
+        transition: transform 0.3s ease;
     }
-#cardTitle{
-    border-radius: 12px;
-}
-    
+
+    .hovered {
+        transform: scale(1.05); /* Adjust the scale factor as per your preference */
+    }
+
+    #cardTitle {
+        border-radius: 12px;
+    }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,178 +1,41 @@
-<!-- <template>
-  <div class="about">
-    <div class="container">
-    <div class="row">
-        <div class="col" >
-            <h1 id="aboutHeading" class="display-4">ABOUT</h1>
-           
-        </div>
-    </div>
-  </div> 
-
-  <div class="container">
-    <div class="row">
-        <div class="col" >
-          <h5 id="aboutHeading2" class="display-7">What is our business goal</h5>
-           
-        </div>
-    </div>
-  </div> 
-
-  <div class="container">
-    <div class="row">
-        <div class="col" >
-          <h5 id="aboutHeading3" class="display-7">What is our business mission</h5>
-           
-        </div>
-    </div>
-  </div> 
-
-
-  <div class="container">
-    <div class="row">
-        <div class="col" >
-          <h5 id="aboutHeading4" class="display-7">What is our business vision</h5>
-           
-        </div>
-    </div>
-  </div> 
-  
-<img id="homeGirl" src="https://i.ibb.co/q5KNX7N/animegirl-removebg-preview.png">
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-5" id="aboutPara">
-            <p>Our business goal is to provide our customers with
-the best quality products that they will be both
-satisfied with the look of our clothing as well as the
- quality of the products that we provide.
-            </p>
-        </div>
-    </div>
-    </div>
-
-    <div class="container">
-    <div class="row">
-        <div class="col-md-5" id="aboutPara2">
-            <p>Ultimately our business goal is to go global and 
-establish different branches all over South Africa so that 
-people from all over the country can have access to our 
-excellent clothing
-            </p>
-        </div>
-    </div>
-    </div>
-
-    <div class="container">
-    <div class="row">
-        <div class="col-md-5" id="aboutPara3">
-            <p>Our business vision is to empower other developers
- and to inspire them. To show others that if they want 
-to start their own business from the ground up then they
- could even if they have no assistance.
-            </p>
-        </div>
-    </div>
-    </div>
-  </div>
-</template>
-  -->
 <template>
   <div class="about container-fluid">
     <div class="row">
-      <div class="col">
-          <div>
-            <h1 id="aboutHeading" class="display-4">ABOUT</h1>
-          </div>
-          <div>
-            <h5 id="aboutHeading2" class="display-7">What is our business goal</h5>
-            <p>Our business goal is to provide our customers with
-the best quality products that they will be both
-satisfied with the look of our clothing as well as the
- quality of the products that we provide.
-            </p>
-          </div>
-          <div>
-            <h5 id="aboutHeading3" class="display-7">What is our business mission</h5>
-            <p>Ultimately our business goal is to go global and 
-establish different branches all over South Africa so that 
-people from all over the country can have access to our 
-excellent clothing
-            </p>
-          </div>
-          <div>
-            <h5 id="aboutHeading4" class="display-7">What is our business vision</h5>
-            <p>Our business vision is to empower other developers
- and to inspire them. To show others that if they want 
-to start their own business from the ground up then they
- could even if they have no assistance.
-            </p>
-          </div>
+      <div class="col-md-6 offset-md-3">
+        <div>
+          <h1 id="aboutHeading" class="display-4 text-center">ABOUT</h1>
+        </div>
+        <div>
+          <h5 id="aboutHeading2" class="display-7">What is our business goal</h5>
+          <p>Our business goal is to provide our customers with the best quality products that they will be both satisfied with the look of our clothing as well as the quality of the products that we provide.</p>
+        </div>
+        <div>
+          <h5 id="aboutHeading3" class="display-7">What is our business mission</h5>
+          <p>Ultimately our business goal is to go global and establish different branches all over South Africa so that people from all over the country can have access to our excellent clothing.</p>
+        </div>
+        <div>
+          <h5 id="aboutHeading4" class="display-7">What is our business vision</h5>
+          <p>Our business vision is to empower other developers and to inspire them. To show others that if they want to start their own business from the ground up then they could even if they have no assistance.</p>
+        </div>
       </div>
-      <div class="col">
-        <img id="homeGirl" src="https://i.ibb.co/q5KNX7N/animegirl-removebg-preview.png" class="img-fluid">
-
+      <div class="col-md-6 order-md-2 d-flex justify-content-center align-items-center">
+        <div class="text-center">
+          <img id="homeGirl" src="https://i.ibb.co/q5KNX7N/animegirl-removebg-preview.png" class="img-fluid bigger-image">
+        </div>
       </div>
     </div>
-
   </div>
 </template>
-    
+
 <script>
-// @ is an alias to /src
-
-
 export default {
- 
 }
 </script>
+
 <style>
-/* #aboutHeading{
-  position: absolute;
-  margin-top:23px;
-  margin-left:180px
-
+.bigger-image {
+  max-width: 100%; /* Ensure the image is always responsive */
+  height: auto; /* Automatically adjust the height based on the width */
+  max-height: 400px; /* Set a maximum height for the image */
 }
-#aboutHeading2{
-  position: absolute;
-  margin-top:140px;
-  margin-left:210px
-
-}
-#aboutHeading3{
-  position: absolute;
-  margin-top:290px;
-  margin-left:210px
-
-}
-#aboutHeading4{
-  position: absolute;
-  margin-top:410px;
-  margin-left:210px
-
-}*/
-#homeGirl{
-  /* position:absolute; */
-  /* margin-left:260px; */
-  /* margin-top:40px; */
-  /* height:530px */  
-  width: 300px;
-}
-/*
-#aboutPara{
-  position:absolute;
-  margin-top:180px;
-  margin-left:10px
-}
-
-#aboutPara2{
-  position:absolute;
-  margin-top:320px;
-  margin-left:10px
-}
-#aboutPara3{
-  position:absolute;
-  margin-top:440px;
-  margin-left:10px
-} */
 </style>
